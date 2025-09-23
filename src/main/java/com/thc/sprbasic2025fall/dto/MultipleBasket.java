f4792202171796def0738ed7fde7bfd6b6637e99
01 <talhwan@hotmail.com>
2025-09-23 19:51:20 +0900

    20250923
    properties, gradle

--- src/main/java/com/thc/sprbasic2025fall/dto/MultipleBasket.java
+++ src/main/java/com/thc/sprbasic2025fall/dto/MultipleBasket.java
@@ -1,14 +1,24 @@
 package com.thc.sprbasic2025fall.dto;
 
-public class MultipleBasket {
-    int a;
-    int b;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+public class MultipleBasket {
+    @Getter int a;
+    @Getter int b;
+
+    /*
     public MultipleBasket() {}
     public MultipleBasket(int a, int b) {
         this.a = a;
         this.b = b;
     }
+
     public int getA() {
         return a;
     }
@@ -20,5 +30,5 @@ public class MultipleBasket {
     }
     public void setB(int b) {
         this.b = b;
-    }
+    }*/
 }
