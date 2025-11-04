627b7f1f9336006181f1a69af65546d17397c36d
01 <talhwan@hotmail.com>
2025-11-04 20:25:05 +0900

    20251104
    file

--- src/main/java/com/thc/sprbasic2025fall/controller/DefaultRestController.java
+++ src/main/java/com/thc/sprbasic2025fall/controller/DefaultRestController.java
@@ -1,14 +1,40 @@
 package com.thc.sprbasic2025fall.controller;
 
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @RequestMapping("/api/default") // 모든 메서드에 앞에 붙는 메핑!!!
 @RestController // 페이지 리턴이 아니라, 객체 리턴할꺼에요!
 public class DefaultRestController {
+
+    @PostMapping("uploadFile")
+    public String uploadFile(MultipartFile file) {
+        System.out.println("file : " + file.getOriginalFilename());
+
+        String finalFileName = null;
+        String path = "/Users/a01/workspace/uploadfile2025/";
+        String fileName = file.getOriginalFilename();
+        File newfile = new File(path);
+        if(!newfile.exists()) {
+            newfile.mkdirs();
+        }
+        Date date = new Date();
+        String temp_date = date.getTime() + "";
+        try{
+            FileCopyUtils.copy(file.getBytes(), new File(path + temp_date + "_" + fileName));
+            finalFileName = temp_date + "_" + fileName;
+        } catch (Exception e){}
+
+        return finalFileName;
+    }
 
 }
--- src/main/java/com/thc/sprbasic2025fall/controller/page/DefaultPageController.java
+++ src/main/java/com/thc/sprbasic2025fall/controller/page/DefaultPageController.java
@@ -1,8 +1,17 @@
 package com.thc.sprbasic2025fall.controller.page;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.apache.commons.io.IOUtils;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.*;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.Map;
 
 @RequestMapping("")
 @Controller// 컨트롤러임을 알리기 위해 붙이는 어노테이션!
@@ -14,31 +23,53 @@ public class DefaultPageController {
         return "index"; //페이지컨트롤러 리턴값! src->resources->templates 아래에 있는 리턴값, index.html을 찾아서 파씽해줌!!
     }
 
-    @RequestMapping("/add")
-    public String add(int a, int b, Model model){
-        //파라미터를 받아볼께요!
-        // ~~~?key1=value1&key2=value2&key3=value3 (url에 이렇게 적혀 있으면 , 파라미터 사용 가능!)
-        System.out.println("DefaultPageController.add()");
-        System.out.println("a : " + a);
-        System.out.println("b : " + b);
-
-        int sum = a + b;
-        System.out.println("sum : " + sum);
-
-        model.addAttribute("sum", sum);
-
-        return "add";
+    @ResponseBody
+    @RequestMapping(value = "/image/{file_name:.+}", method = {RequestMethod.GET,RequestMethod.POST})
+    public byte[] getImage(@PathVariable("file_name") String file_name, HttpServletRequest request) throws Exception {
+        byte[] return_byte = null;
+        String path = "/Users/a01/workspace/uploadfile2025/";
+        //해당 이미지를 byte[]형태로 반환
+        File file = new File(path + file_name);
+        InputStream in = null;
+        try {
+            in = new FileInputStream(file);
+            return_byte = IOUtils.toByteArray(in);
+        } catch (FileNotFoundException e) {
+            //logger.info("FileNotFoundException / file_name : " + file_name);
+            //e.printStackTrace();
+        } catch (IOException e) {
+            //logger.info("IOException / file_name : " + file_name);
+            //e.printStackTrace();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (Exception e) {
+                    //logger.info("final Exception / file_name : " + file_name);
+                }
+            }
+        }
+        return return_byte;
     }
+    @ResponseBody
+    @RequestMapping(value = "/download/{file_name:.+}", method = RequestMethod.GET)
+    public void download(@PathVariable("file_name") String file_name, @RequestParam Map<String, Object> map, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //logger.info("root_path : " + root_path);
+        String path = "/Users/a01/workspace/uploadfile2025/";
+        File file = new File(path + file_name);
 
-    @RequestMapping("/addstring")
-    public String addstring(String a, String b, Model model){
-        //두개의 글자를 입력받아서(url에서) 결합해보세요!
-        System.out.println("DefaultPageController.addstring()");
-        System.out.println("a : " + a);
-        System.out.println("b : " + b);
-        String result = a + b;
-        model.addAttribute("result", result);
-        return "addstring";
+        //여기는 response 에 설정해주는 부분인데, 어려우면 당분간은 패쓰!!
+        String mimeType= URLConnection.guessContentTypeFromName(file_name);		//--- 파일의 mime타입을 확인합니다.
+        if(mimeType==null){			//--- 마임타입이 없을 경우 application/octet-stream으로 설정합니다.
+            mimeType = "application/octet-stream";
+        }
+        response.setContentType(mimeType);	//--- response에 mimetype을 설정합니다.
+        response.setContentLength((int) file.length());
+        response.setHeader("Content-Disposition", "attachment; filename=\""+ URLEncoder.encode(file.getName(), "utf-8") +"\"");
+        //
+
+        InputStream inputStream = new BufferedInputStream(new FileInputStream(file));	//--- inputstream 객체를 얻습니다.
+        FileCopyUtils.copy(inputStream, response.getOutputStream());		//--- inputstream으로 파일을 읽고 outputsream으로 파일을 씁니다.
     }
 
 }
